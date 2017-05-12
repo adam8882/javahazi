d621da427d69f712df04b229a4dba5606940fb0c
adam8882 <naraiadam88@gmail.com>
2017-05-12 22:50:25 +0200

    Telefonrol bugfix

--- src/engine/Field.java
+++ src/engine/Field.java
@@ -211,7 +211,7 @@ public class Field {
 
     public boolean isGameOver() {
         for (int i = 0; i < WIDTH; i++)
-            if (fixmatrix[i][1] != 0)
+            if (fixmatrix[1][i] != 0)
                 return true;
         return false;
     }
